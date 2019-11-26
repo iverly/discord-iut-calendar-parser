fa6a5931ab6265ac05fa477de8d95f17478ccb9f
iverly <iverly.dev@gmail.com>
2019-11-26 16:13:49 +0100

    feat: change activity

--- bot.js
+++ bot.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
-const identifier = require('./identifier');
 const debug = require('debug')('discord');
 
 const client = new Discord.Client();
 
 client.once('ready', () => {
     client.user.setStatus('dnd');
-    client.user.setActivity('github/iverly', {type: 'LISTENING'});
+    client.user.setActivity('github.com/iverly', {type: 'WATCHING'});
 })
 
 module.exports = client;
